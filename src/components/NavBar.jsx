1f04d5d7e183ae2e08f5ba2d65c8e01138cf79f1
shahbazcse <cseshahbaz@gmail.com>
2024-01-28 22:42:02 +0530

    Converted to Tailwind CSS

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,24 +5,24 @@ import { FaChevronDown } from "react-icons/fa";
 
 const NavBar = () => {
     return (
-        <div className="nav">
-            <div className="nav_left">
-                <img src={logo} alt="logo" className="app_logo" />
-                <div className="flex flex-col justify-center align-start font-bold gap-[3px] hidden sm:block">
-                    <div className="app_name_main">Appointr</div>
-                    <div className="app_slogan">Your Appointment Buddy</div>
+        <div className="flex justify-between items-center gap-5 border-b shadow-sm w-full px-[4vw] py-[28px] sticky top-0 backdrop-blur-[8px] z-[999]">
+            <div className="flex justify-center items-center gap-2.5">
+                <img src={logo} alt="logo" className="h-14 w-14" />
+                <div className="flex flex-col justify-center align-start font-bold gap-[2px] hidden sm:block">
+                    <div className="text-2xl tracking-wide">Appointr</div>
+                    <div className="text-[0.8rem]">Your Appointment Buddy</div>
                 </div>
             </div>
-            <div className="nav_right">
-                <div className="nav_link">
+            <div className="flex justify-center items-center gap-6">
+                <div className="flex justify-center items-center gap-2.5 text-lg font-bold cursor-pointer text-[#0a1d56]">
                     <div>Menu</div>
-                    <FaChevronDown className="menu_icon" />
+                    <FaChevronDown className="text-[#378760]" />
                 </div>
-                <div className="nav_link">
+                <div className="flex justify-center items-center gap-2.5 text-lg font-bold cursor-pointer text-[#0a1d56]">
                     <div>Contact Us</div>
                 </div>
-                <div className="nav_link share_btn">
-                    <BiLinkExternal className="share_icon" />
+                <div className="flex justify-center items-center gap-2.5 text-lg font-bold cursor-pointer text-[#378760] border border-[#378760] px-5 py-2 rounded-full hover:text-white hover:bg-[#378760]">
+                    <BiLinkExternal className="h-5 w-5" />
                     <div>Share Link</div>
                 </div>
             </div>
