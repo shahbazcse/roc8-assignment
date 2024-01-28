04bbb3f79166c9a26b55a17802f3951849f6d5f0
shahbazcse <cseshahbaz@gmail.com>
2024-01-28 22:51:39 +0530

    Added Sandwich Button for Mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import logo from "../assets/logo.png";
 import { BiLinkExternal } from "react-icons/bi";
 import { FaChevronDown } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const NavBar = () => {
     return (
         <div className="flex justify-between items-center gap-5 border-b shadow-sm w-full px-[4vw] py-[28px] sticky top-0 backdrop-blur-[8px] z-[999]">
             <div className="flex justify-center items-center gap-2.5">
                 <img src={logo} alt="logo" className="h-14 w-14" />
-                <div className="flex flex-col justify-center align-start font-bold gap-[2px] hidden sm:block">
+                <div className="flex flex-col justify-center align-start font-bold gap-[2px]">
                     <div className="text-2xl tracking-wide">Appointr</div>
                     <div className="text-[0.8rem]">Your Appointment Buddy</div>
                 </div>
             </div>
-            <div className="flex justify-center items-center gap-6">
+            <RxHamburgerMenu onClick={() => { }} className="flex h-9 w-9 mr-2 md:hidden" />
+            <div className="md:flex justify-center items-center gap-6 hidden">
                 <div className="flex justify-center items-center gap-2.5 text-lg font-bold cursor-pointer text-[#0a1d56]">
                     <div>Menu</div>
                     <FaChevronDown className="text-[#378760]" />
