656ce8076ebad47d5c9621a1bcb846b658d73030
shahbazcse <cseshahbaz@gmail.com>
2024-01-28 21:16:15 +0530

    Changed Font

--- src/components/booking/Calendar.jsx
+++ src/components/booking/Calendar.jsx
@@ -16,7 +16,7 @@ export const Calendar = () => {
     }, []);
     return (
         <div className='flex flex-col justify-center gap-4 p-[3vh] h-full w-full bg-[#EBEBF0] rounded-t-xl md:rounded-tl-xl md:rounded-tr-[0]'>
-            <div className='font-[roboto] tracking-wide'>
+            <div className='font-[roboto] tracking-wider'>
                 <div className='text-[2.8vh] text-gray-800 font-bold'>{service.name}</div>
                 <div className='text-gray-600 text-[1.7vh]'><span className='font-bold'>Timezone:</span> {service.timezone}</div>
             </div>
